973ef192f42a5e760fdf6272ab96d7b79b2fd5ab
Jakub Kropáček <kropikuba@gmail.com>
2024-06-04 22:52:17 +0200

    finished moving

--- config_mountpoints.py
+++
@@ -1,6 +0,0 @@
-DOT_CONFIG_DIR = "~/.config"
-
-mountpoints = {
-    "tmux": {"path": f"{DOT_CONFIG_DIR}/tmux", "type": "dir"},
-    "ulauncher": {"path": f"{DOT_CONFIG_DATA}/ulauncher", "type": "dir"},
-}
