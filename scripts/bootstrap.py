f2c8353a6ab1f0cce09fd651563b889adc42d542
Jakub Kropáček <kropikuba@gmail.com>
2024-06-05 18:27:21 +0200

    added pre-commit

---
+++ scripts/bootstrap.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3.12
+import subprocess as sp
+import sys
+import tempfile
+import urllib.request
+from pathlib import Path
+
+VIRTUALENV_PYZ = "https://bootstrap.pypa.io/virtualenv.pyz"
+PIPX_LOCAL_DIR = Path("~/.local/opt/venv").expanduser()
+LOCAL_BIN_DIR = Path("~/.local/bin").expanduser()
+
+
+def get_virtualenv_pyz(dir_path: Path) -> Path:
+    print("Getting virtualenv.pyz")
+    urllib.request.urlretrieve(VIRTUALENV_PYZ, dir_path / "virtualenv.pyz")
+    print("Finished")
+    return dir_path / "virtualenv.pyz"
+
+
+def init_pipx(venv_pyz_path: Path) -> list[Path]:
+    print("Initing pipx")
+    sp.check_output(
+        [sys.executable, venv_pyz_path, PIPX_LOCAL_DIR],
+        stderr=sp.STDOUT,
+    )
+
+    sp.check_output(
+        [
+            Path(PIPX_LOCAL_DIR) / "bin" / "pip",
+            "install", "pipx", "virtualenv",
+        ],
+        stderr=sp.STDOUT,
+    )
+
+    return [
+        Path(PIPX_LOCAL_DIR) / "bin" / "pipx",
+        Path(PIPX_LOCAL_DIR) / "bin" / "virtualenv",
+    ]
+
+
+def symlink_executables(executables: list[Path]):
+    if not LOCAL_BIN_DIR.exists():
+        print(f"Creating {LOCAL_BIN_DIR}")
+        LOCAL_BIN_DIR.mkdir()
+    for executable in executables:
+        print(f"Symlinking {executable} -> {LOCAL_BIN_DIR}")
+        (LOCAL_BIN_DIR / executable.name).symlink_to(executable)
+
+
+def main() -> int:
+    with tempfile.TemporaryDirectory() as dirname:
+        print(f"Created tmpdir -> {dirname}")
+        virtualenv_path = get_virtualenv_pyz(Path(dirname))
+        print(f"Installed temporary virtualenv.pyz to {virtualenv_path}")
+        executables = init_pipx(virtualenv_path)
+    print("Symlinking executables")
+    symlink_executables(executables)
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
