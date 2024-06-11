a289225b89cfb59922fedfbf3f24df45b805555b
Jakub Kropáček <kropikuba@gmail.com>
2024-06-11 19:42:54 +0200

    added package list for the future

--- provision/setup_packages.py
+++ provision/setup_packages.py
@@ -49,3 +49,32 @@ def deploy_packages():
     )
 
     # TODO: add flatpaks too
+
+
+"""
+ansible-core 2.17.0
+argcomplete 3.3.0
+babi 1.5.6
+bandit 1.7.8
+black 24.4.2
+bumpver 2023.1129
+cookiecutter 2.6.0
+invoke 2.2.0
+ipython 8.25.0
+mercurial 6.7.3
+meson 1.4.1
+ninja 1.11.1.1
+notebook 7.2.1
+nox 2024.4.15
+pip-run 12.6.1
+poetry 1.8.3
+pre-commit 3.7.1
+python-lsp-server 1.11.0
+random-scripts 0.2.0
+reorder-python-imports 3.13.0
+ruff 0.4.8
+tox 4.15.1
+uv 0.2.10
+virtualenv 20.26.2
+wlc 1.14
+"""
